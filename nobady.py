830329dad04b635c7045c47540b070cb4c08eb0f
galgalumix <zakksworld1@kumoh.ac.kr>
2025-09-12 14:14:01 +0900

    game install commit

--- nobady.py
+++ nobady.py
@@ -5,3 +5,9 @@ def Kai():
         print("Dead")
         
 print(Kai)
+def game_install():
+    a = int(input("게임을 설치하시겠습니까? :"))
+    if a == 1:
+        print("게임이 설치되고있습니다.")
+    elif a == 0:
+        print("")
