4747f53ccc4657db6dd56f046d60783ede515f13
galgalumix <zakksworld1@kumoh.ac.kr>
2025-09-12 14:14:53 +0900

    del2 commit

--- nobady.py
+++ nobady.py
@@ -1,13 +1,8 @@
-print("nobady here!!")
-a = int(input("??? : "))
-def Kai():
-    if a >=  23 :
-        print("Dead")
-        
+
 print(Kai)
 def game_install():
     a = int(input("게임을 설치하시겠습니까? :"))
     if a == 1:
         print("게임이 설치되고있습니다.")
     elif a == 0:
-        print("")
+        print("")   
